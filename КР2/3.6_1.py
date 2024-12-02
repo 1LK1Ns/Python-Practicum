19e040ecee293b738ae5e6f943767b0b47aff965
ILK1Ns <bakirov.il@list.ru>
2024-12-02 21:52:41 +0300

    optimized_code

--- КР2/3.6_1.py
+++ КР2/3.6_1.py
@@ -1,11 +1,7 @@
 def main():
     for _ in range(int(input())):
-        s = input()
-        a = int(s[:s.index("&")])
-        s = s[s.index("&") + 1:]
-        b = int(s[:s.index("&")])
-        s = s[s.index("&") + 1:]
-        print(s[a::2][:b])
+        s = input().split("&")
+        print(s[2][int(s[0])::2][:int(s[1])])
 
 
 if __name__ == "__main__":
--- КР2/3.6_2.py
+++ КР2/3.6_2.py
@@ -1,11 +1,9 @@
 def main():
     a = dict()
-    s = input()
-    while s != "":
+    while (s := input()) != "":
         w = s.split()
         for i in w:
             a[i[-1].upper()] = a.get(i[-1].upper(), []) + [i.lower()]
-        s = input()
     for i, j in a.items():
         print(f"{i} - {", ".join(sorted(set(j)))}")
 
